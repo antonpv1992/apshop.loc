e095efadbdb508e1fa214b8ff6fd1bc91251bb28
antonpv1992 <antonpv1992@email.ua>
2021-10-05 20:46:53 +0300

    added loading of classes via psr-4

--- public/index.php
+++ public/index.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once dirname(__DIR__). "/Framework/Configs/constants.php";
-//require_once CONFIGS . DS . "autoloader.php";
 require_once "../vendor/autoload.php";
 $routes = require_once CONFIGS . DS . "routes.php";
 
