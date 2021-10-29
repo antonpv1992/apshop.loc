434eeffcc298550418815e0b9a2c32e0e74c1cd2
antonpv1992 <antonpv1992@email.ua>
2021-10-29 10:58:16 +0300

    removed settimeout and moved the page script to a separate file

---
+++ public/scripts/shop-page.js
@@ -0,0 +1,78 @@
+let container = document.querySelector('.goods-list');
+container.innerHTML = renderSpin();
+getAllProducts();
+
+async function getAllProducts() {
+    let data = await fetch('http://apshop.loc/api/shop', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    let result = await data.json();
+    let element = document.querySelector('.goods-list');
+    let template = ""
+    result.forEach(product => {
+        template += singleGoodRender(product);
+    });
+    element.innerHTML = template;
+}
+
+function singleGoodRender(product) {
+    return `
+    <div class="single-good">
+        <a href="/shop/${product.category}/${product.alias}">
+            <img src="assets/images/${product.image}">
+            <h4>${product.title}</h4>
+        </a>
+        <p>
+            <span>${product.oldPrice}</span>
+            <i>${product.price}</i>
+        </p>
+        <div class="characteristics">
+            <div class="single-char">
+                <span>Brand: </span>
+                ${product.brand}
+            </div>
+            <div class="single-char">
+                <span>Type: </span>
+                ${product.category}
+            </div>
+        </div>
+        <a class="check-product">
+            <i class="fa fa-shopping-cart"></i>
+            Add to cart
+        </a>
+    </div>`;
+}
+
+function renderSpin() {
+    let spin = `
+    <div class="spinner">
+        <div class="spinner-row">
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+        </div>
+        <div class="spinner-row">
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+        </div>
+        <div class="spinner-row">
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+        </div>
+        <div class="spinner-row">
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+            <div class="spinner-block"></div>
+        </div>
+    </div>`;
+    return spin;
+}
